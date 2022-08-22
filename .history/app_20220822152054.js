36bfa9de9ce10ee47e7b71742d621e9e149b25bb
ellisvelandia <eyis619@gmail.com>
2022-08-22 17:18:55 -0500

    DOM

---
+++ .history/app_20220822152054.js
@@ -0,0 +1,34 @@
+// const title = document.querySelector("#main-heading");
+
+// title.style.color = "red";
+
+// console.log(title);
+
+// const listItems = document.querySelectorAll(".list-items");
+
+// for (i = 0; i < listItems.length; i++) {
+//   listItems[i].style.fontSize = "2rem";
+// }
+
+// console.log(listItems);
+
+// Creating Element
+
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
+ul.append(li)
+
+// Modifying the text
+
+// const firstListItem = document.querySelector('.list-items');
+
+// console.log (firstListItem.innerText);
+// console.log (firstListItem.textContent); 
+// console.log (firstListItem.innerHTML);
+
+li.innerText = 'x-men';
+
+// Modifying Attributes & classes
+li.classList.add(list)
+
